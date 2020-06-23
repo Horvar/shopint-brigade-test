f66d585758fa59c3bed4725798a5d2c8e9448dfe
Horvar <horselvaren@gmail.com>
2020-06-23 16:58:36 +0300

    Task completed

--- src/js/script.js
+++ src/js/script.js
@@ -2,12 +2,27 @@ import $ from 'jquery';
 
 $('.section-greet__bg').fadeOut(0);
 
+$('.js-delay-short, .js-delay-long').css({
+    'opacity': '0'
+});
+
+
 $(document).ready(function(){
 
     $('.section-greet__bg').fadeIn(500);
 
     setTimeout(() => {
-    }, 250);
+        $('.js-delay-long').css({
+            'opacity': '1',
+            'transition': '1s'
+        });
+    }, 500);
 
+    setTimeout(() => {
+        $('.js-delay-short').css({
+            'opacity': '1',
+            'transition': '1s'
+        });
+    }, 0);
 
 });
